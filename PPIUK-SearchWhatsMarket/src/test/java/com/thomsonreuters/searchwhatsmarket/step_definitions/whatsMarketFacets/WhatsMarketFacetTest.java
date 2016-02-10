944b4ba60a23d398071dcdf687d3f6264683c275
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-02-10 16:29:54 +0000

    Usman - Fixed whats market duplicate step def problem
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C765435

--- PPIUK-SearchWhatsMarket/src/test/java/com/thomsonreuters/searchwhatsmarket/step_definitions/whatsMarketFacets/WhatsMarketFacetTest.java
+++ PPIUK-SearchWhatsMarket/src/test/java/com/thomsonreuters/searchwhatsmarket/step_definitions/whatsMarketFacets/WhatsMarketFacetTest.java
@@ -25,21 +25,6 @@ public class WhatsMarketFacetTest extends BaseStepDef {
     private Integer[] resultArray = new Integer[10];
     private Map<String, Integer> facetsMapWithCountValues;
 
-    @Then("^the user is able to verify that the result in position \"(.*?)\" is whats market content because it contains one of the whats market resource types$")
-    public void theUserIsAbleToVerifyThatTheResultInPositionIsWhatsMarketContentBecauseItContainsOneOfTheWhatsMarketResourceTypes(String resultposition) throws Throwable {
-        String item = whatsMarketSearchResultsPage.getResultItemMetaData(resultposition);
-        assertTrue((item.contains("Administrations")) || (item.contains("Secondary issues")) || (item.contains("Listing Rules transactions"))
-                || (item.contains("Public M&A deals")) || (item.contains("AGMs: FTSE 350:"))
-                || (item.contains("De-listings: AIM")) || (item.contains("Demergers")
-                || (item.contains("IPOs: AIM")) || (item.contains("Joint ventures"))
-                || (item.contains("IPOs: Main Market")) || (item.contains("Reorganisations and schemes"))
-                || (item.contains("Returns of value to shareholders"))
-                || (item.contains("AGMs: AIM:")) || (item.contains("De-listings: Main Market"))
-                || (item.contains("AGMs: AIM 50:")) || (item.contains("Convertible bonds"))
-                || (item.contains("Transfers to the Main Market from AIM"))
-                || (item.contains("Listed company restructurings")) || (item.contains("AIM Rules: Reverse takeovers"))));
-    }
-
     @When("^the user expands the whats market facet \"(.*?)\"$")
     public void theUserExpandsTheWhatsMarketFacet(String arg1) throws Throwable {
         whatsMarketSearchResultsPage.expandWhatsMarketFacet(arg1).click();
