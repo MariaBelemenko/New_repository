59fd02400f3c45d6fac34d8d62a443354dbe8b62
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-02-11 11:38:20 +0000

    Usman - Search Whats Market new run files
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C766198

---
+++ PPIUK-SearchWhatsMarket/src/test/java/com/thomsonreuters/searchwhatsmarket/RunSearchWhatsMarketScopedSearchTest.java
@@ -0,0 +1,16 @@
+package com.thomsonreuters.searchwhatsmarket;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-htmlreport/SearchWhatsMarketScopedSearchReport", "junit:target/junit_cucumber.xml", "json:target/cucumber.json"},
+        features = "src/test/resources/com/thomsonreuters/searchwhatsmarket/features/whatsMarketScopedSearch",
+        tags = {"~@wip", "~@manual"},
+        monochrome = true,
+        snippets = SnippetType.CAMELCASE)
+public class RunSearchWhatsMarketScopedSearchTest {
+}
---
+++ PPIUK-SearchWhatsMarket/src/test/java/com/thomsonreuters/searchwhatsmarket/RunSearchWhatsMarketSearchResultsSortingTest.java
@@ -0,0 +1,16 @@
+package com.thomsonreuters.searchwhatsmarket;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-htmlreport/SearchWhatsMarketSearchResultsSortingReport", "junit:target/junit_cucumber.xml", "json:target/cucumber.json"},
+        features = "src/test/resources/com/thomsonreuters/searchwhatsmarket/features/whatsMarketSearchResultsSorting",
+        tags = {"~@wip", "~@manual"},
+        monochrome = true,
+        snippets = SnippetType.CAMELCASE)
+public class RunSearchWhatsMarketSearchResultsSortingTest {
+}
