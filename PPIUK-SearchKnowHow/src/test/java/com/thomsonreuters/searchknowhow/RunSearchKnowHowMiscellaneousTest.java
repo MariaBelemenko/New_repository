80d967ec221f4125038c4a10b26533bd1643d80c
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-02-15 10:56:28 +0000

    Usman - Adding correct run files for Search KnowHow
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C766964

---
+++ PPIUK-SearchKnowHow/src/test/java/com/thomsonreuters/searchknowhow/RunSearchKnowHowMiscellaneousTest.java
@@ -0,0 +1,16 @@
+package com.thomsonreuters.searchknowhow;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-htmlreport/SearchKnowHowMiscellaneousReport", "junit:target/junit_cucumber.xml", "json:target/cucumber.json"},
+        features = "src/test/resources/com/thomsonreuters/searchknowhow/features/knowHowMiscellaneous",
+        tags = {"~@wip", "~@manual"},
+        monochrome = true,
+        snippets = SnippetType.CAMELCASE)
+public class RunSearchKnowHowMiscellaneousTest {
+}
