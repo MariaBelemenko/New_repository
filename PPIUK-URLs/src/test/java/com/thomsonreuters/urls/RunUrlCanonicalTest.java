457d5c10adc3d61e3a7d87533ae8e15f17c4a581
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-02-20 02:35:15 +0000

    Usman - Url project completed
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C768608

--- PPIUK-URLs/src/test/java/com/thomsonreuters/urls/RunUrlCanonicalTest.java
+++ PPIUK-URLs/src/test/java/com/thomsonreuters/urls/RunUrlCanonicalTest.java
@@ -3,15 +3,22 @@ package com.thomsonreuters.urls;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-htmlreport/UrlCanonicalReport", "junit:target/junit_cucumber.xml", "json:target/cucumber.json"},
         features = "src/test/resources/com/thomsonreuters/urls/features/canonical",
-		tags = {"~@wip", "~@manual"},
+        tags = {"~@wip", "~@manual"},
         monochrome = true,
         snippets = SnippetType.CAMELCASE)
 
 public class RunUrlCanonicalTest {
+
+    @BeforeClass
+    public static void reporting() {
+        System.setProperty("username", "UrlUser1");
+    }
+
 }
--- PPIUK-URLs/src/test/java/com/thomsonreuters/urls/RunUrlKnowHowTest.java
+++ PPIUK-URLs/src/test/java/com/thomsonreuters/urls/RunUrlKnowHowTest.java
@@ -3,15 +3,22 @@ package com.thomsonreuters.urls;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-htmlreport/UrlKnowHowReport", "junit:target/junit_cucumber.xml", "json:target/cucumber.json"},
         features = "src/test/resources/com/thomsonreuters/urls/features/knowHow",
-		tags = {"~@wip", "~@manual"},
+        tags = {"~@wip", "~@manual"},
         monochrome = true,
         snippets = SnippetType.CAMELCASE)
 
 public class RunUrlKnowHowTest {
+
+    @BeforeClass
+    public static void reporting() {
+        System.setProperty("username", "UrlUser2");
+    }
+
 }
