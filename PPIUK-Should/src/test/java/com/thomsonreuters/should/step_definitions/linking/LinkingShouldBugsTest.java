1e6cbccce0065ae36ef240d35174c567503a60e9
Ardenka, Pavel <Pavel.Ardenka@thomsonreuters.com>
2016-03-02 16:42:04 +0000

    Pavel - step defs for SHOULD linking tests
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C772513

--- PPIUK-Should/src/test/java/com/thomsonreuters/should/step_definitions/linking/LinkingShouldBugsTest.java
+++ PPIUK-Should/src/test/java/com/thomsonreuters/should/step_definitions/linking/LinkingShouldBugsTest.java
@@ -1,6 +1,12 @@
 package com.thomsonreuters.should.step_definitions.linking;
 
+import com.thomsonreuters.pageobjects.common.PageActions;
 import com.thomsonreuters.pageobjects.otherPages.NavigationCobalt;
+import com.thomsonreuters.pageobjects.pages.landingPage.PracticalLawUKCategoryPage;
+import com.thomsonreuters.pageobjects.pages.plPlusKnowHowResources.TopicPage;
+import com.thomsonreuters.pageobjects.pages.search.KnowHowDocumentPage;
+import com.thomsonreuters.pageobjects.pages.search.KnowHowSearchResultsPage;
+import com.thomsonreuters.pageobjects.pages.search.SearchResultsPage;
 import com.thomsonreuters.pageobjects.utils.Linking.LinkingUtils;
 import com.thomsonreuters.should.step_definitions.BaseStepDef;
 import com.westgroup.novus.productapi.Novus;
@@ -9,8 +15,14 @@ import com.westgroup.novus.productapi.Relationship;
 import com.westgroup.novus.productapi.RelationshipManager;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 
 import static com.thoughtworks.selenium.SeleneseTestNgHelper.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -20,6 +32,12 @@ public class LinkingShouldBugsTest extends BaseStepDef {
 
     private LinkingUtils Linking = new LinkingUtils();
     private NavigationCobalt navigationCobalt = new NavigationCobalt();
+    private PracticalLawUKCategoryPage practicalLawUKCategoryPage = new PracticalLawUKCategoryPage();
+    private PageActions pageActions = new PageActions();
+    private SearchResultsPage searchResultsPage = new SearchResultsPage();
+    private KnowHowSearchResultsPage knowHowSearchResultsPage = new KnowHowSearchResultsPage();
+    private KnowHowDocumentPage knowHowDocumentPage = new KnowHowDocumentPage();
+    private TopicPage topicPage = new TopicPage();
 
     private String plcRef = null;
     private int NovusRelatedCount = 0;
@@ -153,6 +171,68 @@ public class LinkingShouldBugsTest extends BaseStepDef {
         novus.setResponseTimeout(30000);
         novus.useLatestPit();
         return novus;
+    }
+
+    @When("^the user runs a free text search for the query \"(.*)\"$")
+    public void theUserRunsAFreeTextSearchForTheQuery(String query) throws Throwable {
+        StringSelection stringSelection = new StringSelection(query);
+        Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+        practicalLawUKCategoryPage.searchButton().isDisplayed();
+        practicalLawUKCategoryPage.freeTextField().clear();
+        if (query.contains("(") || query.contains(")") || query.contains("&")) {
+            clpbrd.setContents(stringSelection, null);
+            practicalLawUKCategoryPage.freeTextField().sendKeys(Keys.CONTROL + "v");
+        } else {
+            practicalLawUKCategoryPage.freeTextField().sendKeys(query);
+        }
+        if (practicalLawUKCategoryPage.getClass().equals(ChromeDriver.class)) {
+            pageActions.keyPress(Keys.ENTER);
+        } else {
+            practicalLawUKCategoryPage.searchButton().click();
+        }
+        theUserVerifiesThatTheResultsListPageIsDisplayed();
+    }
+
+    @When("^the user verifies that the results list page is displayed$")
+    public void theUserVerifiesThatTheResultsListPageIsDisplayed() throws Throwable {
+        Robot robot = new Robot();
+        robot.delay(5000);
+        try {
+            searchResultsPage.resultsListHeader().isDisplayed();
+            searchResultsPage.filterHeader().isDisplayed();
+        } catch (Exception e) {
+        }
+    }
+
+    @When("^the user opens the result in position \"(.*?)\"$")
+    public void theUserOpenTheResultInPosition(String resultIndex) {
+        knowHowSearchResultsPage.clickOnResultItem(Integer.parseInt(resultIndex));
+    }
+
+    @When("^the user verifies the presence of the Also Found In heading$")
+    public void theUserVerifiesThePresenceOfTheAlsoFoundInHeading() throws Throwable {
+        knowHowDocumentPage.alsoFoundInHeader().isDisplayed();
+    }
+
+    @When("^the user verifies the presence of a link entitled \"(.*)\"$")
+    public void theUserVerifiesThePresenceOfALinkEntitled(String link) throws Throwable {
+        knowHowDocumentPage.topicPageLink(link).isDisplayed();
+    }
+
+    @When("^the user selects the link entitled \"(.*)\"$")
+    public void theUserSelectsTheLinkEntitled(String link) throws Throwable {
+        knowHowDocumentPage.topicPageLink(link).click();
+    }
+
+    @When("^the user validates that the left hand table of contents is displayed$")
+    public void theUserValidatesThatTheLeftHandTableOfContentsIsDisplayed() throws Throwable {
+        knowHowDocumentPage.tableOfContentLeftHandTable().isDisplayed();
+    }
+
+    @When("^the user verifies that the topic page entitled \"(.*)\" is displayed to the user$")
+    public void theUserVerifiesThatTheTopicPageEntitledIsDisplayedToTheUser(String topic) throws Throwable {
+        String text = topicPage.topicPageTitle().getText();
+        assertTrue(text.equals(topic));
     }
 
 }
