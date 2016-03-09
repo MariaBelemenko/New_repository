abc5adf456b032ceaced0c8614811153adf9b8a7
Bairaagoni, Chiranjeevi <Chiranjeevi.Bairaagoni@thomsonreuters.com>
2016-03-09 10:46:18 +0000

    Chiran: Refactored the excel accessing code as multiple functions are created for same purpose in multiple files, so made into unique place and removed redundant methods
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C774309

--- PPIUK-PageObjects/src/main/java/com/thomsonreuters/pageobjects/common/ExcelFileReader.java
+++ PPIUK-PageObjects/src/main/java/com/thomsonreuters/pageobjects/common/ExcelFileReader.java
@@ -1,13 +1,14 @@
 package com.thomsonreuters.pageobjects.common;
 
-import com.thomsonreuters.driver.exception.PageOperationException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.util.HashMap;
@@ -24,7 +25,7 @@ public final class ExcelFileReader {
 
     private static final String USERS_SHEET_NAME = "Users";
     private static final String EMAILS_SHEET_NAME = "Emails";
-    protected static Map<String, String> excelPasswords;
+    protected static Map<String, Row> excelPasswords;
 
     private static RandomAccessFile raf = null;
 
@@ -49,27 +50,94 @@ public final class ExcelFileReader {
     }
 
     public static String getCobaltEmail(String username) {
-        return getDataFromCobaltUSers(USERS_SHEET_NAME, username, USERS_EMAIL_COLUMN_NUMBER);
+        loadDataIntoMAP();
+        try{
+            return excelPasswords.get(username).getCell(USERS_EMAIL_COLUMN_NUMBER).getStringCellValue();
+        }catch(Exception e){
+            return null;
+        }
+        //return getDataFromCobaltUSers(USERS_SHEET_NAME, username, USERS_EMAIL_COLUMN_NUMBER);
     }
 
     public static String getCobaltPassword(String username) {
+        loadDataIntoMAP();
         try{
-            if(excelPasswords == null || excelPasswords.size() == 0 || !excelPasswords.containsKey(username)) {
-                LOG.info("Calling Excel File Reader to read the password from excel file.");
-                excelPasswords = getPasswords();
-            }
-            return excelPasswords.get(username);
+            return excelPasswords.get(username).getCell(USERS_PASSWORD_COLUMN_NUMBER).getStringCellValue();
         }catch(Exception e){
-            throw new PageOperationException("No user has found in excel sheet for the given name:"+username +e.getMessage());
+            LOG.warn("No password has found in excel sheet for the given name:"+username +e.getMessage());
+            return null;
         }
         //return getDataFromCobaltUSers(USERS_SHEET_NAME, username, USERS_PASSWORD_COLUMN_NUMBER);
     }
+
+    private static void loadDataIntoMAP() {
+        LOG.info("excelPasswords : "+excelPasswords);
+        for (int i = 0; i < 5; i++) {
+            try {
+                if (excelPasswords == null || excelPasswords.size() == 0) {
+                    LOG.info("Calling Excel File Reader to read the password from excel file.");
+                    excelPasswords = getDataFromExcel();
+                }
+                break;
+            } catch (Exception e) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static synchronized Map<String, Row> getDataFromExcel() {
+        LOG.info("Reading passwords into map as key value pairs");
+        Map<String, Row> passwords = new HashMap<String, Row>();
+        String dataFromCell = null;
+        try {
+            FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheet(USERS_SHEET_NAME);
+            int rowsCount = sheet.getLastRowNum();
+            for (int i = 1; i <= rowsCount; i++) {
+                Row row = sheet.getRow(i);
+                passwords.put(row.getCell(0).getStringCellValue(), row);
+            }
+        } catch (IOException e) {
+            LOG.error(IOEXCEPTION, e);
+            passwords = new HashMap<String, Row>();
+        }
+        return passwords;
+    }
+
+    public static String getDefaultUser() /*throws IOException, InterruptedException*/ {
+        return "topsecret1234";
+    }
+
+    public static void unlockUser(String username) /*throws IOException, InterruptedException*/ {
+        LOG.debug("Unlock user code commented out");
+    }
+
 
     public static String getEmailPassword(String email) {
         return getDataFromCobaltUSers(EMAILS_SHEET_NAME, email, EMAILS_PASSWORD_COLUMN_NUMBER);
     }
 
     private static String getDataFromCobaltUSers(String sheetName, String firstColumnValue, int columnNumber) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                return getData(sheetName, firstColumnValue, columnNumber);
+            } catch (Exception e) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return "";
+    }
+
+    private static synchronized String getData(String sheetName, String firstColumnValue, int columnNumber) {
         String dataFromCell = null;
         try {
             FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
@@ -87,173 +155,6 @@ public final class ExcelFileReader {
             LOG.error(IOEXCEPTION, e);
         }
         return dataFromCell;
+
     }
-
-    public static String getDefaultUser() /*throws IOException, InterruptedException*/ {
-        return "topsecret1234";
-        /*try {
-            FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            int rowsCount = sheet.getLastRowNum();
-            for (int i = 0; i <= rowsCount; i++) {
-                Row row = sheet.getRow(i);
-                if (row.getCell(3).getStringCellValue().equalsIgnoreCase("DEFAULT_USER") && !row.getCell(5).getStringCellValue().equalsIgnoreCase("Y")) {
-                    row.getCell(5).setCellValue("Y");
-                    String username = row.getCell(0).toString();
-                    file.close();
-
-                    FileOutputStream outFile = new FileOutputStream(COBALT_USERS_XLSX);
-                    workbook.write(outFile);
-                    outFile.close();
-
-                    return username;
-                }
-            }
-        } catch (IOException e) {
-            throw new IOException("Unable to find Default user \n" + e.getMessage());
-        }
-        throw new IOException("Unable to find Default user");*/
-    }
-
-    public static void unlockUser(String username) /*throws IOException, InterruptedException*/ {
-        LOG.debug("Unlock user code commented out");
-       /* if (null != username) {
-            try {
-                FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
-                XSSFWorkbook workbook = new XSSFWorkbook(file);
-                XSSFSheet sheet = workbook.getSheetAt(0);
-                int rowsCount = sheet.getLastRowNum();
-                for (int i = 0; i <= rowsCount; i++) {
-                    Row row = sheet.getRow(i);
-                    if (row.getCell(0).getStringCellValue().equalsIgnoreCase(username)) {
-                        row.getCell(5).setCellValue("N");
-                        file.close();
-
-                        FileOutputStream outFile = new FileOutputStream(COBALT_USERS_XLSX);
-                        workbook.write(outFile);
-                        outFile.close();
-                    }
-                }
-            } catch (IOException e) {
-                throw new IOException("Unable to unlock Default user " + username + "\n " + e.getMessage());
-            }
-        } else {
-            LOG.info("UserName is null. No need to update the ExcelSheet");
-        }*/
-    }
-
-    public void setBrowserCounts() {
-        for (int i = 0; i < 5; i++) {
-            try {
-                setBrowserCount();
-            } catch (Exception e) {
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public void reduceBrowserCounts() {
-        for (int i = 0; i < 5; i++) {
-            try {
-                removeBrowserCount();
-            } catch (Exception e) {
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public void getBrowserCounts() {
-        for (int i = 0; i < 5; i++) {
-            try {
-                getBrowserCount();
-            } catch (Exception e) {
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private synchronized int getBrowserCount() {
-        try {
-            FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            XSSFSheet sheet = workbook.getSheetAt(2);
-            return sheet.getLastRowNum();
-        } catch (IOException e) {
-            return 0;
-        }
-    }
-
-    private synchronized void removeBrowserCount() {
-        try {
-            FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            XSSFSheet sheet = workbook.getSheetAt(2);
-            int rowsCount = sheet.getLastRowNum();
-            for (int i = 0; i <= rowsCount; i++) {
-                Row row = sheet.getRow(rowsCount + 1);
-                row.getCell(0).setCellValue("");
-
-                FileOutputStream outFile = new FileOutputStream(COBALT_USERS_XLSX);
-                workbook.write(outFile);
-                outFile.close();
-
-            }
-        } catch (IOException e) {
-            throw new PageOperationException("Unable to write to excel file \n" + e.getMessage());
-        }
-    }
-
-    private synchronized void setBrowserCount() throws FileNotFoundException {
-        try {
-            FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            XSSFSheet sheet = workbook.getSheetAt(2);
-            int rowsCount = sheet.getLastRowNum();
-            for (int i = 0; i <= rowsCount; i++) {
-                Row row = sheet.getRow(rowsCount + 1);
-                row.getCell(0).setCellValue("Y");
-
-                FileOutputStream outFile = new FileOutputStream(COBALT_USERS_XLSX);
-                workbook.write(outFile);
-                outFile.close();
-
-            }
-        } catch (IOException e) {
-            throw new PageOperationException("Unable to write to excel file \n" + e.getMessage());
-        }
-    }
-
-    public static Map<String, String> getPasswords() {
-        LOG.info("Reading passwords into map as key value pairs");
-        Map<String, String> passwords = new HashMap<String, String>();
-        String dataFromCell = null;
-        try {
-            FileInputStream file = new FileInputStream(COBALT_USERS_XLSX);
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            XSSFSheet sheet = workbook.getSheet(USERS_SHEET_NAME);
-            int rowsCount = sheet.getLastRowNum();
-            for (int i = 1; i <= rowsCount; i++) {
-                Row row = sheet.getRow(i);
-                passwords.put(row.getCell(0).getStringCellValue(), row.getCell(1).getStringCellValue());
-            }
-        } catch (IOException e) {
-            LOG.error(IOEXCEPTION, e);
-            passwords = new HashMap<String, String>();
-        }
-        return passwords;
-    }
-
 }
