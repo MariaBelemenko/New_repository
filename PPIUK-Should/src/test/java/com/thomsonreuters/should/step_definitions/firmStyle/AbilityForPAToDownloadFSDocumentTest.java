282d5c7913def908a9df41ff2f8c83f65d11972b
Smolina, Iryna <iryna.smolina@thomsonreuters.com>
2016-03-28 14:27:04 +0000

    Iryna - stabilization fs should tests
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C780152

--- PPIUK-Should/src/test/java/com/thomsonreuters/should/step_definitions/firmStyle/AbilityForPAToDownloadFSDocumentTest.java
+++ PPIUK-Should/src/test/java/com/thomsonreuters/should/step_definitions/firmStyle/AbilityForPAToDownloadFSDocumentTest.java
@@ -1,12 +1,24 @@
 package com.thomsonreuters.should.step_definitions.firmStyle;
 
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.List;
+
+import com.thomsonreuters.pageobjects.common.FileActions;
 import com.thomsonreuters.pageobjects.otherPages.NavigationCobalt;
 import com.thomsonreuters.pageobjects.pages.plPlusResearchDocDisplay.document.PracticalLawToolsPage;
 import com.thomsonreuters.pageobjects.pages.plPlusResearchDocDisplay.document.StandardDocumentPage;
 import com.thomsonreuters.pageobjects.pages.widgets.CategoryPage;
 import com.thomsonreuters.should.step_definitions.BaseStepDef;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.assertj.core.api.SoftAssertions;
 
 public class AbilityForPAToDownloadFSDocumentTest extends BaseStepDef {
@@ -15,6 +27,47 @@ public class AbilityForPAToDownloadFSDocumentTest extends BaseStepDef {
     private StandardDocumentPage standardDocumentPage = new StandardDocumentPage();
     private PracticalLawToolsPage practicalLawToolsPage = new PracticalLawToolsPage();
     private NavigationCobalt navigationCobalt = new NavigationCobalt();
+    private FileActions fileActions = new FileActions();
+    
+    private final static String DOWNLOADED_FILE_PATH = System.getProperty("user.home") + "/Downloads";
+    private File downloadedFile = null;
+    
+    @When("^the user deletes all files with name \"([^\"]*)\" and extension \"([^\"]*)\" from Downloads$")
+    public void deleteFilesFormDownloads(String name, String extension) throws Throwable {
+        File dir = new File(DOWNLOADED_FILE_PATH);
+        List<File> files = (List<File>) FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, null);
+        for (File file : files) {
+            if (file.getName().contains(name) && file.getName().contains(extension)) {
+                file.delete();
+            }
+        }
+    }
+    
+    @Then("^the file \"([^\"]*)\" should be downloaded to the users machine$")
+    public void fileShouldDownloadToTheUsersMachine(String name) throws Throwable {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_ESCAPE);
+        robot.keyRelease(KeyEvent.VK_ESCAPE);
+
+        downloadedFile = fileActions.findFile(name, DOWNLOADED_FILE_PATH);
+        assertTrue("File was not downloaded", downloadedFile != null && downloadedFile.exists());
+    }
+
+    @Then("^the file \"([^\"]*)\" should be removed$")
+    public void deleteDownloadedDocFile(String name) throws Throwable {
+        fileActions.deleteFile(downloadedFile);
+    }
+    
+    @Then("\"([^\"]*)\" page is displayed$")
+    public void loginPageIsDisplayed(String pageUrl) throws Throwable {
+        standardDocumentPage.waitForPageToLoad();
+        assertTrue("'" + pageUrl + "' page is not displayed", standardDocumentPage.getCurrentUrl().contains(pageUrl));
+    }
 
     @When("^the user opens \"([^\"]*)\" link$")
     public void openLinkFromList(String linkName) throws Throwable {
@@ -48,4 +101,14 @@ public class AbilityForPAToDownloadFSDocumentTest extends BaseStepDef {
         navigationCobalt.waitForPageToLoadAndJQueryProcessingWithCustomTimeOut(90);
     }
 
+	@When("the user clicks on FS download link \"([^\"]*)\"")
+	public void clickDownloadFSFile(String linkName) throws Throwable {
+		standardDocumentPage.downloadFSFile(linkName).click();
+	}
+
+	@When("the user clicks on Home page")
+	public void clickHomePage() throws Throwable {
+		standardDocumentPage.homePage().click();
+		standardDocumentPage.waitForPageToLoad();
+	}
 }
