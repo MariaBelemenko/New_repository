d21797bb14e096ec8cb0461f6226aeed8f7b74a6
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-03-31 12:44:09 +0000

    Usman - HP Changes
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C781307

--- PPIUK-AssetPages/src/test/java/com/thomsonreuters/assetpages/RunAssetPagesPageTest.java
+++ PPIUK-AssetPages/src/test/java/com/thomsonreuters/assetpages/RunAssetPagesPageTest.java
@@ -17,8 +17,13 @@ public class RunAssetPagesPageTest {
 
     @BeforeClass
     public static void reporting() {
-        System.setProperty("username", "AssetPageUser5");
-        System.setProperty("password", "Password1");
+        if (System.getProperty("username").equals("None")) {
+            System.setProperty("username", "AssetPageUser5");
+            System.setProperty("password", "Password1");
+        }
+        else {
+            System.out.println("Username is pre-defined in the Run Command as: " + System.getProperty("username"));
+        }
     }
 
 }
