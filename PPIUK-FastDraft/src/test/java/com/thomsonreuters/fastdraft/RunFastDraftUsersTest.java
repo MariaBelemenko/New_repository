2f53f128f2fbfe81e05a1872438957237b304e7e
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-03-31 12:46:43 +0000

    Usman - HP Changes
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C781310

--- PPIUK-FastDraft/src/test/java/com/thomsonreuters/fastdraft/RunFastDraftUsersTest.java
+++ PPIUK-FastDraft/src/test/java/com/thomsonreuters/fastdraft/RunFastDraftUsersTest.java
@@ -17,8 +17,13 @@ public class RunFastDraftUsersTest {
 
     @BeforeClass
     public static void reporting() {
-        System.setProperty("username", "FastDraftUser4");
-        System.setProperty("password", "Password1");
+        if (System.getProperty("username").equals("None")) {
+            System.setProperty("username", "FastDraftUser4");
+            System.setProperty("password", "Password1");
+        }
+        else {
+            System.out.println("Username is pre-defined in the Run Command as: " + System.getProperty("username"));
+        }
     }
 
 }
