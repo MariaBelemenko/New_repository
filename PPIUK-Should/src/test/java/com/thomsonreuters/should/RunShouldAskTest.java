569698a8958ea1481590d7b64385a27e96fb8e3e
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-03-31 13:35:53 +0000

    Usman - HP Changes
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C781349

--- PPIUK-Should/src/test/java/com/thomsonreuters/should/RunShouldAskTest.java
+++ PPIUK-Should/src/test/java/com/thomsonreuters/should/RunShouldAskTest.java
@@ -17,8 +17,13 @@ public class RunShouldAskTest {
 
     @BeforeClass
     public static void reporting() {
-        System.setProperty("username", "AskTestuser");
-        System.setProperty("password", "TestUser2015");
+        if (System.getProperty("username").equals("None")) {
+            System.setProperty("username", "AskTestuser");
+            System.setProperty("password", "TestUser2015");
+        }
+        else {
+            System.out.println("Username is pre-defined in the Run Command as: " + System.getProperty("username"));
+        }
     }
 
 }
--- PPIUK-Should/src/test/java/com/thomsonreuters/should/RunShouldLinkingTest.java
+++ PPIUK-Should/src/test/java/com/thomsonreuters/should/RunShouldLinkingTest.java
@@ -17,8 +17,13 @@ public class RunShouldLinkingTest {
 
     @BeforeClass
     public static void reporting() {
-        System.setProperty("username", "LinkingUser1");
-        System.setProperty("password", "Password1");
+        if (System.getProperty("username").equals("None")) {
+            System.setProperty("username", "LinkingUser1");
+            System.setProperty("password", "Password1");
+        }
+        else {
+            System.out.println("Username is pre-defined in the Run Command as: " + System.getProperty("username"));
+        }
     }
 
 }
