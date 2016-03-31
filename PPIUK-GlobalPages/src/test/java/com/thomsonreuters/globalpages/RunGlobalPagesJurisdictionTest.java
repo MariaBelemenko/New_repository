0da5fc108f6d64e182b3b67fb32197bcb4f1c03c
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-03-31 12:59:09 +0000

    usman - HP Changes
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C781319

--- PPIUK-GlobalPages/src/test/java/com/thomsonreuters/globalpages/RunGlobalPagesJurisdictionTest.java
+++ PPIUK-GlobalPages/src/test/java/com/thomsonreuters/globalpages/RunGlobalPagesJurisdictionTest.java
@@ -17,8 +17,13 @@ public class RunGlobalPagesJurisdictionTest {
 
     @BeforeClass
     public static void reporting() {
-        System.setProperty("username", "GlobalPagesUser3");
-        System.setProperty("password", "Password1");
+        if (System.getProperty("username").equals("None")) {
+            System.setProperty("username", "GlobalPagesUser3");
+            System.setProperty("password", "Password1");
+        }
+        else {
+            System.out.println("Username is pre-defined in the Run Command as: " + System.getProperty("username"));
+        }
     }
 
 }
--- PPIUK-GlobalPages/src/test/java/com/thomsonreuters/globalpages/step_definitions/CommonLoginNaviagtionSteps.java
+++ PPIUK-GlobalPages/src/test/java/com/thomsonreuters/globalpages/step_definitions/CommonLoginNaviagtionSteps.java
@@ -1,22 +1,5 @@
 package com.thomsonreuters.globalpages.step_definitions;
 
-import static com.thomsonreuters.pageobjects.utils.CobaltUser.isUserFirstUser;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotVisibleException;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-import org.springframework.util.StringUtils;
-
 import com.thomsonreuters.pageobjects.common.CommonMethods;
 import com.thomsonreuters.pageobjects.common.ExcelFileReader;
 import com.thomsonreuters.pageobjects.common.PageActions;
@@ -33,18 +16,23 @@ import com.thomsonreuters.pageobjects.pages.plcLegacy.PLCLegacyHeader;
 import com.thomsonreuters.pageobjects.pages.plcLegacy.PLCLegacyLoginScreen;
 import com.thomsonreuters.pageobjects.pages.search.SearchHomePage;
 import com.thomsonreuters.pageobjects.rest.service.impl.RestServiceFFHImpl;
-import com.thomsonreuters.pageobjects.utils.CobaltUser;
-import com.thomsonreuters.pageobjects.utils.OnepassLoginUtils;
-import com.thomsonreuters.pageobjects.utils.Product;
-import com.thomsonreuters.pageobjects.utils.Routing;
-import com.thomsonreuters.pageobjects.utils.RoutingPage;
-import com.thomsonreuters.pageobjects.utils.User;
+import com.thomsonreuters.pageobjects.utils.*;
 import com.thomsonreuters.pageobjects.utils.folders.FoldersUtils;
-
 import cucumber.api.Transpose;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.thomsonreuters.pageobjects.utils.CobaltUser.isUserFirstUser;
 
 /**
  * Login and Navigation Steps.
@@ -140,7 +128,7 @@ public class CommonLoginNaviagtionSteps extends BaseStepDef {
         navigationCobalt.navigateToPLUKPlus();
         plcHomePage.closeCookieConsentMessage();
         resetCurrentUser();
-        if (!baseUrl.contains("prod")) {
+        if (!baseUrl.contains("hotprod")) {
             theUserClicksOnSignOnLinkOnTheHeader();
         } else {
             LOG.info("OpenWeb is OFF on production. User already on login page");
@@ -690,14 +678,20 @@ public class CommonLoginNaviagtionSteps extends BaseStepDef {
          * Then just remove the below lines.
          */
         switch (baseUrl) {
+            case "hotprod":
+                LOG.info("HOT PROD Site is being tested.");
+                break;
             case "prod":
                 LOG.info("Production Site is being tested.");
+                wlnHeader.signInLink().click();
                 break;
             case "prodA":
                 LOG.info("PROD A is being tested.");
+                wlnHeader.signInLink().click();
                 break;
             case "prodB":
                 LOG.info("PROD B is being tested.");
+                wlnHeader.signInLink().click();
                 break;
             default:
                 wlnHeader.signInLink().click();
