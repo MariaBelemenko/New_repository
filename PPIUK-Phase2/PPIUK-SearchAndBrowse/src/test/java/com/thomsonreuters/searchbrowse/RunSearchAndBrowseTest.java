ff56c7cea2384a4639d017ee3cf296b89a85cae4
Bairaagoni, Chiranjeevi <Chiranjeevi.Bairaagoni@thomsonreuters.com>
2016-04-12 09:25:20 +0000

    Chiran: Renamed the Phase 2 projects Research, Search &Browse projects and added the folder structure
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C784724

---
+++ PPIUK-Phase2/PPIUK-SearchAndBrowse/src/test/java/com/thomsonreuters/searchbrowse/RunSearchAndBrowseTest.java
@@ -0,0 +1,24 @@
+package com.thomsonreuters.searchbrowse;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-htmlreport/report1", "junit:target/junit_cucumber.xml", "json:target/json-files/report1.json"},
+        features = "",
+        monochrome = true,
+        tags = {"~@wip", "~@manual"},
+        snippets = SnippetType.CAMELCASE)
+public class RunSearchAndBrowseTest {
+
+    @BeforeClass
+    public static void reporting(){
+
+    }
+
+}
+
