552163e37eda7829810639504009b9a96b282f13
Hussain, Usman <usman.hussain@thomsonreuters.com>
2016-04-12 08:36:49 +0000

    Usman - no need for utils witin AskRe-Write
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C784708

--- PPIUK-AskReWrite/src/main/java/com/thomsonreuters/askRewrite/utils/ScreenShot/ScreenShotUtil.java
+++
@@ -1,7 +0,0 @@
-//package com.thomsonreuters.askRewrite.utils.ScreenShot;
-//
-//import com.thomsonreuters.driver.framework.AbstractPage;
-//
-//public class ScreenShotUtil extends AbstractPage {
-//
-//}
