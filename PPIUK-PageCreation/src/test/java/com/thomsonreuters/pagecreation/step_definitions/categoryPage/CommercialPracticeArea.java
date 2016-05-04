d111d9e438612f003905f9f3b7cbd21a348c85f8
Harper, Phil (Legal) <phil.harper@thomsonreuters.com>
2016-05-04 11:01:33 +0000

    CPET Page Creation Step Def file
    git-tfs-id: [http://tfsnpt.int.thomson.com:8080/tfs/Cobalt_Collection]$/Cobalt QED Testing/PLPLusUK;C790989

---
+++ PPIUK-PageCreation/src/test/java/com/thomsonreuters/pagecreation/step_definitions/categoryPage/CommercialPracticeArea.java
@@ -0,0 +1,34 @@
+package com.thomsonreuters.pagecreation.step_definitions.categoryPage;
+
+import com.thomsonreuters.pagecreation.step_definitions.BaseStepDef;
+import com.thomsonreuters.pageobjects.common.CommonMethods;
+import com.thomsonreuters.pageobjects.pages.landingPage.CommercialPracticeAreaHomePage;
+import com.thomsonreuters.pageobjects.pages.landingPage.PracticeAreaLandingPage;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+/**Phil Harper, SD file for tests for CPET Commercial Practice Area
+ * Created by U0162413 on 08/04/2016.
+ */
+public class CommercialPracticeArea extends BaseStepDef {
+    PracticeAreaLandingPage PLPracticeArea = new PracticeAreaLandingPage();
+    private CommonMethods commonMethods = new CommonMethods();
+    CommercialPracticeAreaHomePage CommercialPAHome = new CommercialPracticeAreaHomePage();
+
+    @Given("^the user navigates to the PL website \"(.*?)\"$")
+    public void theUserNavigatesToThePLWebsite(String website) throws Throwable {
+        commonMethods.navigate(website);
+    }
+
+    @Given("^navigates to the Practice Area \"(.*?)\"$")
+    public void navigatesToTheCommercialPracticeArea(String linkText) throws Throwable {
+        PLPracticeArea.genericLink(linkText).click();
+    }
+
+    @When("^the Tab is selected \"(.*?)\"$")
+    public void theTopicsTabIsSelected(String linkText) throws Throwable {
+        CommercialPAHome.menuTab(linkText).click();
+    }
+
+}
+
